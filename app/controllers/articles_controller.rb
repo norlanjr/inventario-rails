e98b1fe83f2c63cabdbcdcccd277d81d631adbfc
Norlan Rivera <njrivera9364@gmail.com>
2017-11-20 11:07:38 -0600

    agregue mas detalles a la vista

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,9 +14,12 @@ class ArticlesController < ApplicationController
         @article = Article.new
         
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
     #POST /articlesse accede por ,edio dde un post
     def create
-        @article =Article.new({nombre: params[:article][:nombre], precio: params[:article][:precio]})
+        @article =Article.new(article_params)
 
         if @article.save
         redirect_to @article 
@@ -29,6 +32,19 @@ class ArticlesController < ApplicationController
         @article.destroy
         redirect_to articles_path
         # Cambio
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render :edit
+        end 
+
+    end
+    private
+    def article_params
+        params.require(:article).permit(:nombre,:precio)
     end
     
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,9 @@ class Article < ApplicationRecord
     #presence:true lo k va hacer es validar
     # de que no este vacio no c pueda huardar vacio
     validates :nombre, presence: true, uniqueness: true
+    validates :descripcion, presence: true
     validates :precio, presence: true
+    validates :precio2, presence: true
+    validates :costo, presence: true
+
 end
